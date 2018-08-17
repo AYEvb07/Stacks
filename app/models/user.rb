b03535d9e04cc4f7a70b96e46d9e528321b5e3fb
Dana Terry <dlterry14@gmail.com>
2018-08-17 22:56:28 +0000

    Updates some routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
 
   belongs_to :course  
-  enum role: [:user, :student, :admin]
+  enum role: [:user, :teacher, :admin]
 
   after_initialize :set_default_role, :if => :new_record?
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   def set_default_role
-    self.role ||= :student
+    self.role ||= :user
   end
 
   devise :database_authenticatable, :registerable,
