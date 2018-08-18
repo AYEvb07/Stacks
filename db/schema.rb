6c18d65da031c42d2e28d3b42dbcbc73c1f283f0
Arisa Vaughns <arisa.vaughns@gmail.com>
2018-08-18 19:08:07 +0000

    Ran migration AddQuizToCourses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180817000850) do
+ActiveRecord::Schema.define(version: 20180818190719) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
