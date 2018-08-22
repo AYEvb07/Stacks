58fefceea0e66f83b112d3d385139a5c2eb117f1
Dana Terry <dlterry14@gmail.com>
2018-08-22 21:55:57 +0000

    updated controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   
+  root to: 'static#home'  
+    
   resources :dashboards
   get 'dashboard/user_dashboard'
   get 'index' => 'dashboards#index'
 
   root to: 'static#home'
   get 'team' => 'static#team'
-  get 'home' => 'static#home'
+#   get 'home' => 'static#home'
   get 'new' => 'static#new'
   get 'login' => 'login#home'
   get 'dashboards' => 'static#dashboards'
